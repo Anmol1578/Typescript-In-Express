cb693e0d85d1eeb32c91319bae2ad37b59b5944b
Anmol1578 <anmol10562@gmail.com>
2026-08-16 19:58:25 +0530

    Completed -
    Add validation middleware for numeric pet ID and apply it to the pet retrieval route

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import type { Router } from "express";
 import { getPets, getPetbyId } from "../controllers/pets.controllers.js";
 
+import {validateNumericId} from '../middleware/pets.middleware.js'
+
 const petRouter: Router = express.Router();
 
 // GET route for the home page
 petRouter.get("/", getPets);
 
-petRouter.get("/:id", getPetbyId);
+petRouter.get("/:id", validateNumericId,  getPetbyId);
 
 export default petRouter;
